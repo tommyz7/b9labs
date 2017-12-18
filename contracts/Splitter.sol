7d321812ac491bfc230b4aa094499f8540e4e8b6
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2017-12-17 22:28:04 -0500

    refactor Splitter contract and tests

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -6,9 +6,20 @@ import './Pausible.sol';
 
 contract Splitter is Pausible {
 
-    mapping(address => uint256) public balances;
+    mapping(address => uint256) private balances;
 
-    event LogSplit(address addr1, address addr2, uint256 amount);
+    event LogSplit(
+        address indexed addr1,
+        address indexed addr2,
+        uint256 amount,
+        address indexed sender
+    );
+
+    event LogWithdraw(address indexed beneficiary, uint256 amount);
+
+    function getBalance(address addr) public view returns(uint256) {
+        return balances[addr];
+    }
 
     function splitFunds(address addr1, address addr2)
         public
@@ -16,7 +27,7 @@ contract Splitter is Pausible {
         onlyIfRunning
         returns(bool)
     {
-        // require splitable amount
+        // if amount not splitable, return the difference to sender
         if (msg.value % 2 == 1) {
             balances[msg.sender] += 1;
         }
@@ -25,7 +36,7 @@ contract Splitter is Pausible {
         balances[addr1] += half;
         balances[addr2] += half;
 
-        LogSplit(addr1, addr2, half);
+        LogSplit(addr1, addr2, half, msg.sender);
         return true;
     }
 
@@ -34,6 +45,8 @@ contract Splitter is Pausible {
         uint256 value = balances[msg.sender];
         balances[msg.sender] = 0;
         msg.sender.transfer(value);
+
+        LogWithdraw(msg.sender, value);
         return true;
     }
 }
--- test/splitter.js
+++ test/splitter.js
@@ -1,53 +1,75 @@
-var Splitter = artifacts.require('./Splitter.sol');
+const Splitter = artifacts.require('./Splitter.sol');
+const Promise = require('bluebird');
+const isTxSuccessful = require('../utils/isTxSuccessful');
 
+if (typeof web3.eth.getBlockPromise !== "function") {
+    Promise.promisifyAll(web3.eth, { suffix: "Promise" });
+}
 
 contract('Splitter', function(accounts) {
-    var spl;
+    let spl, bob, alice, carol;
 
-    beforeEach(async () => {
+    before("deploy new Splitter", async () => {
         spl = await Splitter.new({from: accounts[0]});
+        bob = accounts[2];
+        alice = accounts[3];
+        carol = accounts[4];
     });
 
 
     it('should split ether between peers', async () => {
-        var bob = accounts[2];
-        var alice = accounts[3];
-        var carol = accounts[4];
+        var tx = await spl.splitFunds(bob, carol, {from: alice, value: 21, gas: 200000});
+        assert(isTxSuccessful(tx, 200000), "Split transaction failed.");
 
-        tx = await spl.splitFunds(bob, carol, {from: alice, value: 21});
-        assert.equal(tx.receipt.status, 1, "Split transaction failed.");
-
-        var bobWBal = await spl.balances.call(bob);
-        var carolWBal = await spl.balances.call(carol);
-        var aliceWBal = await spl.balances.call(alice);
+        var bobWBal = await spl.getBalance.call(bob);
+        var carolWBal = await spl.getBalance.call(carol);
+        var aliceWBal = await spl.getBalance.call(alice);
 
         assert.equal(bobWBal.toNumber(), 10, "Bob share incorrect.");
         assert.equal(carolWBal.toNumber(), 10, "Carol share incorrect.");
         assert.equal(aliceWBal.toNumber(), 1, "Alice share incorrect.");
+    });
 
-        // withdraw funds
-        var bobBal = await web3.eth.getBalance(bob);
-        var tx = await spl.withdraw({from: bob, gasPrice: web3.toWei(1, "gwei")});
-        assert.equal(tx.receipt.status, 1, "Bob Withdraw transaction failed.");
-        var txCost = web3.toWei(1, "gwei") * tx.receipt.gasUsed;
-        var bobNewBal = await web3.eth.getBalance(bob);
+    it('should allow to withdraw funds', async () => {
+        var bobWBal = await spl.getBalance.call(bob);
+        var carolWBal = await spl.getBalance.call(carol);
+        var aliceWBal = await spl.getBalance.call(alice);
+
+        // withdraw bob
+        var bobBal = await web3.eth.getBalancePromise(bob);
+        var tx = await spl.withdraw({from: bob, gas: 200000});
+        assert(isTxSuccessful(tx, 200000), "Bob Withdraw transaction failed.");
+        var gasUsed = tx.receipt.gasUsed;
+        tx = web3.eth.getTransaction(tx.tx);
+        var gasPrice = tx.gasPrice.toNumber();
+
+        var txCost = gasPrice * gasUsed;
+        var bobNewBal = await web3.eth.getBalancePromise(bob);
         assert(bobNewBal.plus(txCost).minus(bobWBal).toNumber(), bobBal.toNumber(), "Bob after withdraw balance incorrect.");
 
-        var carolBal = await web3.eth.getBalance(carol);
-        var tx = await spl.withdraw({from: carol, gasPrice: web3.toWei(1, "gwei")});
-        assert.equal(tx.receipt.status, 1, "carol Withdraw transaction failed.");
-        var txCost = web3.toWei(1, "gwei") * tx.receipt.gasUsed;
-        var carolNewBal = await web3.eth.getBalance(carol);
+        // withdraw carol
+        var carolBal = await web3.eth.getBalancePromise(carol);
+        var tx = await spl.withdraw({from: carol, gas: 200000});
+        assert(isTxSuccessful(tx, 200000), "carol Withdraw transaction failed.");
+        var gasUsed = tx.receipt.gasUsed;
+        tx = web3.eth.getTransaction(tx.tx);
+        var gasPrice = tx.gasPrice.toNumber();
+
+        var txCost = gasPrice * gasUsed;
+        var carolNewBal = await web3.eth.getBalancePromise(carol);
         assert(carolNewBal.plus(txCost).minus(carolWBal).toNumber(), carolBal.toNumber(), "Carol after withdraw balance incorrect.");
 
-        var aliceBal = await web3.eth.getBalance(alice);
-        var tx = await spl.withdraw({from: alice, gasPrice: web3.toWei(1, "gwei")});
-        assert.equal(tx.receipt.status, 1, "alice Withdraw transaction failed.");
-        var txCost = web3.toWei(1, "gwei") * tx.receipt.gasUsed;
-        var aliceNewBal = await web3.eth.getBalance(alice);
+        // withdraw alice
+        var aliceBal = await web3.eth.getBalancePromise(alice);
+        var tx = await spl.withdraw({from: alice, gas: 2000000});
+        assert(isTxSuccessful(tx, 200000), "alice Withdraw transaction failed.");
+        var gasUsed = tx.receipt.gasUsed;
+        tx = web3.eth.getTransaction(tx.tx);
+        var gasPrice = tx.gasPrice.toNumber();
+
+        var txCost = gasPrice * gasUsed;
+        var aliceNewBal = await web3.eth.getBalancePromise(alice);
         assert(aliceNewBal.plus(txCost).minus(aliceWBal).toNumber(), aliceBal.toNumber(), "alice after withdraw balance incorrect.");
-
-
     });
 
     it('should be pausible only by owner', async () => {
@@ -62,7 +84,6 @@ contract('Splitter', function(accounts) {
         assert.equal(isRunning, false, "isRunning 2 incorrect.");
         try {
             tx = await spl.splitFunds(bob, carol, {from: alice, value: 21});
-            assert(false, "Revert expected");
         } catch(e) {
             tx = await spl.resume({from: accounts[0]});
             isRunning = await spl.isRunning.call()
